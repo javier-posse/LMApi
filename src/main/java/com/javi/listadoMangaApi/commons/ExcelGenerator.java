db31a84d258aa0043e3728483aa4bc80b95ed367
Javier <javierpossel@gmail.com>
2024-12-09 23:14:40 +0100

    Añadida hoja de estadísticas al excel, con gráfico y datos

--- src/main/java/com/javi/listadoMangaApi/commons/ExcelGenerator.java
+++ src/main/java/com/javi/listadoMangaApi/commons/ExcelGenerator.java
@@ -3,11 +3,15 @@ package com.javi.listadoMangaApi.commons;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.DataValidation;
 import org.apache.poi.ss.usermodel.DataValidationConstraint;
 import org.apache.poi.ss.usermodel.DataValidationHelper;
@@ -21,7 +25,17 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.ss.util.CellRangeAddressList;
 import org.apache.poi.ss.util.RegionUtil;
+import org.apache.poi.xddf.usermodel.chart.ChartTypes;
+import org.apache.poi.xddf.usermodel.chart.LegendPosition;
+import org.apache.poi.xddf.usermodel.chart.XDDFChartLegend;
+import org.apache.poi.xddf.usermodel.chart.XDDFDataSource;
+import org.apache.poi.xddf.usermodel.chart.XDDFDataSourcesFactory;
+import org.apache.poi.xddf.usermodel.chart.XDDFNumericalDataSource;
+import org.apache.poi.xddf.usermodel.chart.XDDFPieChartData;
+import org.apache.poi.xssf.usermodel.XSSFChart;
+import org.apache.poi.xssf.usermodel.XSSFClientAnchor;
 import org.apache.poi.xssf.usermodel.XSSFDataValidationHelper;
+import org.apache.poi.xssf.usermodel.XSSFDrawing;
 import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -37,14 +51,14 @@ public class ExcelGenerator {
 	    throws GenericException {
 
 	Workbook excel = new XSSFWorkbook();
-	Sheet sheet = excel.createSheet("Lanzamientos");
-	sheet.setColumnWidth(0, 17000);
-	sheet.setColumnWidth(1, 10000);
-	sheet.setColumnWidth(2, 10000);
-	sheet.setColumnWidth(3, 10000);
+	Sheet sheetReleases = excel.createSheet("Lanzamientos");
+	sheetReleases.setColumnWidth(0, 17000);
+	sheetReleases.setColumnWidth(1, 10000);
+	sheetReleases.setColumnWidth(2, 10000);
+	sheetReleases.setColumnWidth(3, 10000);
 
 	// Headers de la tabla y sus estilo
-	Row header = sheet.createRow(0);
+	Row header = sheetReleases.createRow(0);
 
 	CellStyle headerStyle = excel.createCellStyle();
 	headerStyle.setFillForegroundColor(IndexedColors.AQUA.getIndex());
@@ -90,16 +104,16 @@ public class ExcelGenerator {
 	// Validaciones para crear el drop-down
 	String[] options = { "Tomo único", "Primer tomo", "Último tomo" };
 
-	DataValidationHelper validationHelper = new XSSFDataValidationHelper((XSSFSheet) sheet);
+	DataValidationHelper validationHelper = new XSSFDataValidationHelper((XSSFSheet) sheetReleases);
 	DataValidationConstraint constraint = validationHelper.createExplicitListConstraint(options);
 	CellRangeAddressList addressList = new CellRangeAddressList(1, seriesReleases.size(), 3, 3);
 	DataValidation validation = validationHelper.createValidation(constraint, addressList);
 
-	sheet.addValidationData(validation);
+	sheetReleases.addValidationData(validation);
 
 	for (int i = 0; i < seriesReleases.size(); i++) {
 
-	    Row row = sheet.createRow(i + 1);
+	    Row row = sheetReleases.createRow(i + 1);
 
 	    Cell cell = row.createCell(0);
 	    cell.setCellValue(seriesReleases.get(i).getName());
@@ -128,26 +142,106 @@ public class ExcelGenerator {
 	CellRangeAddress regionBodyLeft = new CellRangeAddress(1, seriesReleases.size(), 0, 0);
 	CellRangeAddress regionBodyCenterLeft = new CellRangeAddress(1, seriesReleases.size(), 1, 1);
 	CellRangeAddress regionBodyCenterRight = new CellRangeAddress(1, seriesReleases.size(), 2, 2);
-	CellRangeAddress regionBodyRight = new CellRangeAddress(1, seriesReleases.size(), 2, 2);
+	CellRangeAddress regionBodyRight = new CellRangeAddress(1, seriesReleases.size(), 3, 3);
 
 	// borders body
-	RegionUtil.setBorderTop(BorderStyle.THIN, regionBodyLeft, sheet);
-	RegionUtil.setBorderBottom(BorderStyle.THIN, regionBodyLeft, sheet);
-	RegionUtil.setBorderLeft(BorderStyle.THIN, regionBodyLeft, sheet);
-	RegionUtil.setBorderRight(BorderStyle.THIN, regionBodyLeft, sheet);
-	RegionUtil.setBorderTop(BorderStyle.THIN, regionBodyCenterLeft, sheet);
-	RegionUtil.setBorderBottom(BorderStyle.THIN, regionBodyCenterLeft, sheet);
-	RegionUtil.setBorderLeft(BorderStyle.THIN, regionBodyCenterLeft, sheet);
-	RegionUtil.setBorderRight(BorderStyle.THIN, regionBodyCenterLeft, sheet);
-	RegionUtil.setBorderTop(BorderStyle.THIN, regionBodyCenterRight, sheet);
-	RegionUtil.setBorderBottom(BorderStyle.THIN, regionBodyCenterRight, sheet);
-	RegionUtil.setBorderLeft(BorderStyle.THIN, regionBodyCenterRight, sheet);
-	RegionUtil.setBorderRight(BorderStyle.THIN, regionBodyCenterRight, sheet);
-	RegionUtil.setBorderTop(BorderStyle.THIN, regionBodyRight, sheet);
-	RegionUtil.setBorderBottom(BorderStyle.THIN, regionBodyRight, sheet);
-	RegionUtil.setBorderLeft(BorderStyle.THIN, regionBodyRight, sheet);
-	RegionUtil.setBorderRight(BorderStyle.THIN, regionBodyRight, sheet);
+	RegionUtil.setBorderTop(BorderStyle.THIN, regionBodyLeft, sheetReleases);
+	RegionUtil.setBorderBottom(BorderStyle.THIN, regionBodyLeft, sheetReleases);
+	RegionUtil.setBorderLeft(BorderStyle.THIN, regionBodyLeft, sheetReleases);
+	RegionUtil.setBorderRight(BorderStyle.THIN, regionBodyLeft, sheetReleases);
+	RegionUtil.setBorderTop(BorderStyle.THIN, regionBodyCenterLeft, sheetReleases);
+	RegionUtil.setBorderBottom(BorderStyle.THIN, regionBodyCenterLeft, sheetReleases);
+	RegionUtil.setBorderLeft(BorderStyle.THIN, regionBodyCenterLeft, sheetReleases);
+	RegionUtil.setBorderRight(BorderStyle.THIN, regionBodyCenterLeft, sheetReleases);
+	RegionUtil.setBorderTop(BorderStyle.THIN, regionBodyCenterRight, sheetReleases);
+	RegionUtil.setBorderBottom(BorderStyle.THIN, regionBodyCenterRight, sheetReleases);
+	RegionUtil.setBorderLeft(BorderStyle.THIN, regionBodyCenterRight, sheetReleases);
+	RegionUtil.setBorderRight(BorderStyle.THIN, regionBodyCenterRight, sheetReleases);
+	RegionUtil.setBorderTop(BorderStyle.THIN, regionBodyRight, sheetReleases);
+	RegionUtil.setBorderBottom(BorderStyle.THIN, regionBodyRight, sheetReleases);
+	RegionUtil.setBorderLeft(BorderStyle.THIN, regionBodyRight, sheetReleases);
+	RegionUtil.setBorderRight(BorderStyle.THIN, regionBodyRight, sheetReleases);
 
+	// Generado hoja de estadisticas
+	Sheet sheetStatistics = excel.createSheet("Estadísticas");
+	sheetStatistics.setColumnWidth(0, 15000);
+	sheetStatistics.setColumnWidth(1, 10000);
+	sheetStatistics.setColumnWidth(2, 10000);
+	sheetStatistics.setColumnWidth(3, 10000);
+	sheetStatistics.setColumnWidth(4, 10000);
+	sheetStatistics.setColumnWidth(5, 10000);
+
+	// total de lanzamientos
+	Row row = sheetStatistics.createRow(1);
+	Cell cellReleasesLabel = row.createCell(0);
+	cellReleasesLabel.setCellValue("Total lanzamientos: ");
+	cellReleasesLabel.setCellStyle(headerStyle);
+	Cell cellReleasesCount = row.createCell(1);
+	cellReleasesCount.setCellValue(seriesReleases.size());
+
+	// tabla de editoriales
+	// Contar las apariciones de cada editorial en el primer sheet
+	Map<String, Integer> publisherCounts = new HashMap<>();
+	for (Row rowPublishers : sheetReleases) {
+	    Cell cell = rowPublishers.getCell(2);
+	    if (cell != null && cell.getCellType() == CellType.STRING) {
+		String publisher = cell.getStringCellValue();
+		publisherCounts.put(publisher, publisherCounts.getOrDefault(publisher, 0) + 1);
+	    }
+	}
+	// Ordeno las editoriales de más a menos por número de apariciones
+	List<Map.Entry<String, Integer>> sortedEditorialCounts = new ArrayList<>(publisherCounts.entrySet());
+	sortedEditorialCounts.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+	// Colocar en el sheet las editoriales
+	int rowIndex = 3;
+	Row headerRow = sheetStatistics.createRow(rowIndex++);
+	headerRow.createCell(0).setCellValue("Nombre editorial");
+	headerRow.createCell(1).setCellValue("Cantidad de lanzamientos");
+
+	for (Map.Entry<String, Integer> entry : sortedEditorialCounts) {
+	    Row rowPublishersTable = sheetStatistics.createRow(rowIndex++);
+	    rowPublishersTable.createCell(0).setCellValue(entry.getKey());
+	    rowPublishersTable.createCell(1).setCellValue(entry.getValue());
+	}
+	// total de tomos unicos y primeros tomos
+	int onlyVolumeCont = 0;
+	int firstVolumeCont = 0;
+	for (SeriesReleaseDto serie : seriesReleases) {
+	    if (serie.isOnlyVolume()) {
+		onlyVolumeCont++;
+	    }
+	    if (serie.isFirstRelease()) {
+		firstVolumeCont++;
+	    }
+	}
+	Cell cellOnlyVolumeLabel = row.createCell(2);
+	cellOnlyVolumeLabel.setCellValue("Total tomos únicos: ");
+	cellOnlyVolumeLabel.setCellStyle(headerStyle);
+	Cell cellOnlyVolumeCount = row.createCell(3);
+	cellOnlyVolumeCount.setCellValue(onlyVolumeCont);
+
+	Cell cellFirstVolumeLabel = row.createCell(4);
+	cellFirstVolumeLabel.setCellValue("Total nuevas series: ");
+	cellFirstVolumeLabel.setCellStyle(headerStyle);
+	Cell cellFirstVolumeCount = row.createCell(5);
+	cellFirstVolumeCount.setCellValue(firstVolumeCont);
+
+	// Generacion de grafico editoriales
+	XSSFDrawing drawing = (XSSFDrawing) sheetStatistics.createDrawingPatriarch();
+	XSSFClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, 2, 3, 4, 12);
+	XSSFChart chart = drawing.createChart(anchor);
+	chart.setTitleText("Distribución de Editoriales");
+	chart.setTitleOverlay(false);
+	XDDFChartLegend legend = chart.getOrAddLegend();
+	legend.setPosition(LegendPosition.RIGHT);
+	XDDFDataSource<String> categories = XDDFDataSourcesFactory.fromStringCellRange((XSSFSheet) sheetStatistics,
+		new CellRangeAddress(4, rowIndex - 1, 0, 0));
+	XDDFNumericalDataSource<Double> values = XDDFDataSourcesFactory
+		.fromNumericCellRange((XSSFSheet) sheetStatistics, new CellRangeAddress(4, rowIndex - 1, 1, 1));
+	XDDFPieChartData data = (XDDFPieChartData) chart.createData(ChartTypes.PIE, null, null);
+	XDDFPieChartData.Series series = (XDDFPieChartData.Series) data.addSeries(categories, values);
+	series.setTitle("Editoriales", null);
+	chart.plot(data);
 	// cierre y guardado del fichero
 	File currDir = new File("./src/main/resources/Generated Excel");
 	String path = currDir.getAbsolutePath();
